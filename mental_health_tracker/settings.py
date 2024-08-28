fb9ed4222d687aa2a6ac989b6d9a4dec9052c91f
tjioedava <tjioedava@gmail.com>
2024-08-28 15:43:42 +0700

    adding remote third party host as allowed host

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#rji9jj22mc=(aka=!=zf3v08+shqr3_v_+n$7sezryb-1$vm$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'dava-hannas-mentalhealthtracker.pbp.cs.ui.ac.id']
 
 
 # Application definition
