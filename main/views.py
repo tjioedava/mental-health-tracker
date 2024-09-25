e6391f55664f4d26d5d88457ffbc9825f64f3995
tjioedava <tjioedava@gmail.com>
2024-09-25 15:26:52 +0700

    setting the last login cookie max age the same as session ID cookie (two weeks). only showing products of current user in data delivery

--- main/views.py
+++ main/views.py
@@ -15,7 +15,7 @@ import datetime
 def home(request):
     context = {
         'name': request.user.username,
-        'mood_entries': MoodEntry.objects.filter(user = request.user),
+        'mood_entries': MoodEntry.objects.filter(user=request.user),
         'last_login': request.COOKIES['last_login']
     }
 
@@ -26,7 +26,7 @@ def create_mood_entry(request):
     form = MoodEntryForm(request.POST or None)
 
     if request.method == 'POST' and form.is_valid():
-        mood_entry = form.save(commit = False)
+        mood_entry = form.save(commit=False)
         mood_entry.user = request.user
         mood_entry.save()
         return redirect('main:home')
@@ -37,22 +37,22 @@ def create_mood_entry(request):
 
 @login_required(login_url='/login-user')
 def show_xml(request):
-    mood_entries = MoodEntry.objects.all()
+    mood_entries = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize('xml', mood_entries), content_type='application/xml')
 
 @login_required(login_url='/login-user')
 def show_json(request):
-    mood_entries = MoodEntry.objects.all()
+    mood_entries = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize('json', mood_entries), content_type='application/json')
 
 @login_required(login_url='/login-user')
 def show_xml_by_id(request, id):
-    mood_entry = MoodEntry.objects.filter(pk = id)
+    mood_entry = MoodEntry.objects.filter(user=request.user, pk = id)
     return HttpResponse(serializers.serialize('xml', mood_entry), content_type='application/xml')
 
 @login_required(login_url='/login-user')
 def show_json_by_id(request, id):
-    mood_entry = MoodEntry.objects.filter(pk = id)
+    mood_entry = MoodEntry.objects.filter(user=request.user, pk = id)
     return HttpResponse(serializers.serialize('json', mood_entry), content_type='application/json')
 
 def register(request):
@@ -76,7 +76,7 @@ def login_user(request):
             user = form.get_user()
             login(request, user)
             response = HttpResponseRedirect(request.GET.get('next', reverse('main:home')))
-            response.set_cookie('last_login', str(datetime.datetime.now()))
+            response.set_cookie('last_login', str(datetime.datetime.now()), 60 * 60 * 24 * 7) #set for two weeks in parallel of session ID cookie
             return response
         messages.error(request, 'Incorrect username or password')
 
